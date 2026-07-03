b27a262be4b781df92ed3aa925fc04be154d5554
yaeju <gimleni593@gmail.com>
2026-07-03 19:53:27 +0900

    Change raw source path

--- generate.js
+++ generate.js
@@ -30,7 +30,7 @@ function create_template_source(title, content_file) {
         </pre>
 
         <script type="module">
-            const res = await fetch("${repository_url}" + "/contents/" + "${content_file}")
+            const res = await fetch("https://raw.githubusercontent.com/yaeju1205/blog/main/contents/" + "${content_file}")
             const text = await res.text()
 
             document.querySelector("pre").textContent = text
